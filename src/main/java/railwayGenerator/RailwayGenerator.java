98c9431ae687f9aa0465c92b5fa8d8a3e4f0506a
Gustavo Inkotte <gus.inkotte@gmail.com>
2023-10-09 19:26:58 -0300

    segmentação de trechos de via a partir do perfil vertical, fix ID dinamico e calculo de KM

--- src/main/java/railwayGenerator/RailwayGenerator.java
+++ src/main/java/railwayGenerator/RailwayGenerator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -22,9 +22,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.w3c.dom.Document;
 
-import java.util.Random;
-
 public class RailwayGenerator {
+	
+	int id = 1;
+	int km = 240;
+	int countPontos = 1;
+	
 	public void execute(RailwayGeneratorRequest req) throws Exception {
 		org.jsoup.nodes.Document doc = Jsoup.parse(new File(req.getFilePath()));
 
@@ -33,15 +36,15 @@ public class RailwayGenerator {
 
 		ArrayList<Element> aclivesArr = new ArrayList<Element>();
 		ArrayList<Element> declivesArr = new ArrayList<Element>();
-		ArrayList<Element> planosArr = new ArrayList<Element>();
-		ArrayList<Element> cristasArr = new ArrayList<Element>();
+//		ArrayList<Element> planosArr = new ArrayList<Element>();
+//		ArrayList<Element> cristasArr = new ArrayList<Element>();
 
 		Elements listaDePontos = doc.getElementsByTag("listaDePontosDeMedida").first()
 				.getElementsByTag("pontoDeMedida");
 
 		for (Element ponto : listaDePontos) {
 //			Elements raioCurva = ponto.getElementsByTag("raioCurva");
-			Elements quantidadeRampas = ponto.getElementsByTag("rampa");
+//			Elements quantidadeRampas = ponto.getElementsByTag("rampa");
 			double grauRampa = Double.parseDouble(ponto.getElementsByTag("rampa").first().text());
 
 //			if (raioCurva.size() > 1) {
@@ -50,31 +53,35 @@ public class RailwayGenerator {
 //				retas.add(ponto);
 //			}
 
-			if (quantidadeRampas.size() > 1) {
-				cristasArr.add(ponto);
-			} else if (grauRampa > 0) {
+//			if (quantidadeRampas.size() > 1) {
+//				cristasArr.add(ponto);
+//			} else 
+			
+			if (grauRampa >= 0) {
 				aclivesArr.add(ponto);
 			} else if (grauRampa < 0) {
 				declivesArr.add(ponto);
-			} else {
-				planosArr.add(ponto);
-			}
+			} 
+//			else if (grauRampa == 0.0){
+//				planosArr.add(ponto);
+//			}
 
 		}
-//		System.out.println("Quantidade de retas: " + retas.size());
-//		System.out.println("Quantidade de curvas: " + curvas.size());
 		System.out.println("Quantidade de aclives: " + aclivesArr.size());
 		System.out.println("Quantidade de declives: " + declivesArr.size());
-		System.out.println("Quantidade de planos: " + planosArr.size());
-		System.out.println("Quantidade de cristas: " + cristasArr.size());
+//		System.out.println("Quantidade de planos: " + planosArr.size());
+//		System.out.println("Quantidade de cristas: " + cristasArr.size());
 
 		ArrayList<Element> aclives = generatePms(aclivesArr);
 		ArrayList<Element> declives = generatePms(declivesArr);
-		ArrayList<Element> planos = generatePms(planosArr);
-		ArrayList<Element> cristas = generatePms(cristasArr);
+//		ArrayList<Element> planos = generatePms(planosArr);
+//		ArrayList<Element> cristas = new ArrayList<Element>();
+		
+		System.out.println("Quantidade de trechos de aclive: " + aclives.size());
+		System.out.println("Quantidade de trechos de declive: " + declives.size());
 
-		writeToFile(aclives, declives, planos, cristas, req);
-
+		writeToFile(aclives, declives, null, null, req);
+		
 		System.out.println("Via gerada.");
 	}
 
@@ -96,13 +103,13 @@ public class RailwayGenerator {
 			throw new Exception("Quantidade de declives maior do que o valor existente: " + declives.size());
 		}
 
-		if (req.getQtdPlanos() > planos.size()) {
-			throw new Exception("Quantidade de planos maior do que o valor existente: " + planos.size());
-		}
+//		if (req.getQtdPlanos() > planos.size()) {
+//			throw new Exception("Quantidade de planos maior do que o valor existente: " + planos.size());
+//		}
 
-		if (req.getQtdCristas() > cristas.size()) {
-			throw new Exception("Quantidade de cristas maior do que o valor existente: " + cristas.size());
-		}
+//		if (req.getQtdCristas() > cristas.size()) {
+//			throw new Exception("Quantidade de cristas maior do que o valor existente: " + cristas.size());
+//		}
 
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -115,8 +122,15 @@ public class RailwayGenerator {
 
 		ArrayList<Element> novaVia = generateNewArray(aclives, declives, planos, cristas, req);
 
+		
+//		for (Element trecho : novaVia) {
+//			writePontoDeMedida(trecho, listaDePontosDeMedida, doc, novaVia);
+//			
+//			novaVia.remove(trecho);
+//		}
 		for (int i = 0; i < novaVia.size(); i++) {
-			writePontoDeMedida(novaVia.get(i), listaDePontosDeMedida, doc, Integer.toString(i + 1));
+			writePontoDeMedida(novaVia.get(i), listaDePontosDeMedida, doc, novaVia);
+			novaVia.remove(novaVia.get(i));
 		}
 
 		try (FileOutputStream output = new FileOutputStream(
@@ -150,70 +164,90 @@ public class RailwayGenerator {
 		return listaDePontosDeMedida;
 	}
 
-	public void writePontoDeMedida(Element pmLista, org.w3c.dom.Element listaDePontosDeMedida, Document doc,
-			String id) {
+	public void writePontoDeMedida(Element pmLista, org.w3c.dom.Element listaDePontosDeMedida, Document doc, ArrayList<Element> novaVia) {
 
-		Element ponto = pmLista.getElementsByTag("listaDePontosDeMedida").first();
-		org.w3c.dom.Element pontoDeMedida = doc.createElement("pontoDeMedida");
-		listaDePontosDeMedida.appendChild(pontoDeMedida);
+		Elements listaPontos = pmLista.getElementsByTag("listaDePontosDeMedida").first().getElementsByTag("pontoDeMedida");
+		
 
-		org.w3c.dom.Element idEl = doc.createElement("id");
-		idEl.setTextContent(id);
-		pontoDeMedida.appendChild(idEl);
+		for (Element ponto : listaPontos) {
+			org.w3c.dom.Element pontoDeMedida = doc.createElement("pontoDeMedida");
+			listaDePontosDeMedida.appendChild(pontoDeMedida);
 
-		org.w3c.dom.Element velocidadeMax = doc.createElement("velocidadeMax");
-		velocidadeMax.setTextContent(ponto.getElementsByTag("velocidadeMax").first().text());
-		pontoDeMedida.appendChild(velocidadeMax);
+			org.w3c.dom.Element idEl = doc.createElement("id");
+			idEl.setTextContent(Integer.toString(id));
+			pontoDeMedida.appendChild(idEl);
+			id++;
 
-		org.w3c.dom.Element km = doc.createElement("km");
-		km.setTextContent(ponto.getElementsByTag("km").first().text());
-		pontoDeMedida.appendChild(km);
+			org.w3c.dom.Element velocidadeMax = doc.createElement("velocidadeMax");
+			velocidadeMax.setTextContent(ponto.getElementsByTag("velocidadeMax").first().text());
+			pontoDeMedida.appendChild(velocidadeMax);
+			
+			
+			if(countPontos > 50) {
+				countPontos = 1;
+				km++;
+			}
+			countPontos++;
+			
+			org.w3c.dom.Element km = doc.createElement("km");
+			km.setTextContent(km.toString());
+			pontoDeMedida.appendChild(km);
 
-		try {
 			org.w3c.dom.Element rampa = doc.createElement("rampa");
-			rampa.setAttribute("fim", ponto.getElementsByTag("rampa").first().attr("fim"));
-			rampa.setAttribute("ini", ponto.getElementsByTag("rampa").first().attr("ini"));
+			rampa.setAttribute("ini", "0.0");
+			rampa.setAttribute("fim", "20.0");
 			rampa.setTextContent(ponto.getElementsByTag("rampa").first().text());
 			pontoDeMedida.appendChild(rampa);
-		} catch (Exception e) {
+			
+			for(int i = 1; i <= ponto.getElementsByTag("raioCurva").size(); i++) {
+				org.w3c.dom.Element raioCurva = doc.createElement("raioCurva");
+				raioCurva.setAttribute("fim", ponto.getElementsByTag("raioCurva").get(i-1).attr("fim"));
+				raioCurva.setAttribute("ini", ponto.getElementsByTag("raioCurva").get(i-1).attr("ini"));
+				raioCurva.setTextContent(ponto.getElementsByTag("raioCurva").get(i-1).text());
+				pontoDeMedida.appendChild(raioCurva);
+			}
+				
+			org.w3c.dom.Element ac = doc.createElement("ac");
+			ac.setAttribute("fim", ponto.getElementsByTag("ac").first().attr("fim"));
+			ac.setAttribute("ini", ponto.getElementsByTag("ac").first().attr("ini"));
+			ac.setTextContent(ponto.getElementsByTag("ac").first().text());
+			pontoDeMedida.appendChild(ac);
+
+			org.w3c.dom.Element g20 = doc.createElement("g20");
+			g20.setAttribute("fim", ponto.getElementsByTag("g20").first().attr("fim"));
+			g20.setAttribute("ini", ponto.getElementsByTag("g20").first().attr("ini"));
+			g20.setTextContent(ponto.getElementsByTag("g20").first().text());
+			pontoDeMedida.appendChild(g20);
+
+			org.w3c.dom.Element altitude = doc.createElement("altitude");
+			altitude.setAttribute("fim", ponto.getElementsByTag("altitude").first().attr("fim"));
+			altitude.setAttribute("ini", ponto.getElementsByTag("altitude").first().attr("ini"));
+			altitude.setTextContent(ponto.getElementsByTag("altitude").first().text());
+			pontoDeMedida.appendChild(altitude);
+
+			org.w3c.dom.Element localizacao = doc.createElement("localizacao");
+			pontoDeMedida.appendChild(localizacao);
+
+			org.w3c.dom.Element latitude = doc.createElement("latitude");
+			latitude.setTextContent(
+					ponto.getElementsByTag("localizacao").first().getElementsByTag("latitude").first().text());
+			localizacao.appendChild(latitude);
+
+			try {
+				org.w3c.dom.Element longitude = doc.createElement("longitude");
+				longitude.setTextContent(
+						ponto.getElementsByTag("localizacao").first().getElementsByTag("longitude").first().text());
+				localizacao.appendChild(longitude);
+			} catch (Exception e) {
+				System.err.println(ponto);
+			}
+
+			
+//			if(ponto.getElementsByTag("rampa").size() > 1) {
+//				String novaRampa = ponto.getElementsByTag("rampa").get(1).text();
+//				forceNextPm(novaRampa, listaDePontosDeMedida, doc);
+//			}
 		}
-
-		org.w3c.dom.Element raioCurva = doc.createElement("raioCurva");
-		raioCurva.setAttribute("fim", ponto.getElementsByTag("raioCurva").first().attr("fim"));
-		raioCurva.setAttribute("ini", ponto.getElementsByTag("raioCurva").first().attr("ini"));
-		raioCurva.setTextContent(ponto.getElementsByTag("raioCurva").first().text());
-		pontoDeMedida.appendChild(raioCurva);
-
-		org.w3c.dom.Element ac = doc.createElement("ac");
-		ac.setAttribute("fim", ponto.getElementsByTag("ac").first().attr("fim"));
-		ac.setAttribute("ini", ponto.getElementsByTag("ac").first().attr("ini"));
-		ac.setTextContent(ponto.getElementsByTag("ac").first().text());
-		pontoDeMedida.appendChild(ac);
-
-		org.w3c.dom.Element g20 = doc.createElement("g20");
-		g20.setAttribute("fim", ponto.getElementsByTag("g20").first().attr("fim"));
-		g20.setAttribute("ini", ponto.getElementsByTag("g20").first().attr("ini"));
-		g20.setTextContent(ponto.getElementsByTag("g20").first().text());
-		pontoDeMedida.appendChild(g20);
-
-		org.w3c.dom.Element altitude = doc.createElement("altitude");
-		altitude.setAttribute("fim", ponto.getElementsByTag("altitude").first().attr("fim"));
-		altitude.setAttribute("ini", ponto.getElementsByTag("altitude").first().attr("ini"));
-		altitude.setTextContent(ponto.getElementsByTag("altitude").first().text());
-		pontoDeMedida.appendChild(altitude);
-
-		org.w3c.dom.Element localizacao = doc.createElement("localizacao");
-		pontoDeMedida.appendChild(localizacao);
-
-		org.w3c.dom.Element latitude = doc.createElement("latitude");
-		latitude.setTextContent(
-				ponto.getElementsByTag("localizacao").first().getElementsByTag("latitude").first().text());
-		localizacao.appendChild(latitude);
-
-		org.w3c.dom.Element longitude = doc.createElement("longitude");
-		longitude.setTextContent(
-				ponto.getElementsByTag("localizacao").first().getElementsByTag("longitude").first().text());
-		localizacao.appendChild(longitude);
 	}
 
 	private void writeXml(Document doc, OutputStream output) throws TransformerException {
@@ -235,32 +269,24 @@ public class RailwayGenerator {
 		Random random = new Random();
 
 		for (int i = 0; i < req.getQtdAclives(); i++) {
-			arr.add(aclives.get(random.nextInt(declives.size())));
+			arr.add(aclives.get(random.nextInt(aclives.size())));
 		}
 
 		for (int i = 0; i < req.getQtdDeclives(); i++) {
 			arr.add(declives.get(random.nextInt(declives.size())));
 		}
 
-		for (int i = 0; i < req.getQtdPlanos(); i++) {
-			arr.add(planos.get(random.nextInt(planos.size())));
-		}
-
-		for (int i = 0; i < req.getQtdCristas(); i++) {
-			arr.add(planos.get(random.nextInt(cristas.size())));
-		}
-
-//		for(int i = 0; i < req.getQtdRetas(); i++)
-//		{
-//			arr.add(retas.get(random.nextInt(retas.size())));
+//		for (int i = 0; i < req.getQtdPlanos(); i++) {
+//			arr.add(planos.get(random.nextInt(planos.size())));
 //		}
 
-//		for(int i = 0; i < req.getQtdCurvas(); i++)
-//		{
-//			arr.add(curvas.get(random.nextInt(curvas.size())));
+//		for (int i = 0; i < req.getQtdCristas(); i++) {
+//			arr.add(cristas.get(random.nextInt(cristas.size())));
 //		}
 
 		Collections.shuffle(arr);
+		
+		
 
 		return arr;
 	}
@@ -268,7 +294,7 @@ public class RailwayGenerator {
 	private ArrayList<Element> generatePms(ArrayList<Element> listaPontos) {
 		Element tmpEl = new Element("listaDePontosDeMedida");
 		ArrayList<Element> finalArr = new ArrayList<Element>();
-		String rampaAnterior = listaPontos.get(0).getElementsByTag("rampa").first().text();
+		String rampaAnterior = listaPontos.get(0).getElementsByTag("rampa").first().text();;
 
 		for (Element ponto : listaPontos) {
 			String grauRampa = ponto.getElementsByTag("rampa").first().text();
@@ -278,14 +304,25 @@ public class RailwayGenerator {
 			} else {
 				finalArr.add(tmpEl);
 				tmpEl = new Element("listaDePontosDeMedida");
-//				tmpEl.empty();
 				ponto.appendTo(tmpEl);
 			}
-
 			rampaAnterior = grauRampa;
 		}
 
-		System.out.println(finalArr);
+//		System.out.println(finalArr);
 		return finalArr;
 	}
+	
+//	private void forceNextPm(String novaRampa, org.w3c.dom.Element listaDePontosDeMedida, Document doc) {
+//		for (int i = 0; i < novaVia.size(); i++) {
+//			Elements listaPontos = novaVia.get(i).getElementsByTag("listaDePontosDeMedida").first().getElementsByTag("pontoDeMedida");
+//			String rampa = listaPontos.first().getElementsByTag("rampa").first().text();
+//			
+//			if(rampa.equalsIgnoreCase(novaRampa)) {
+//				Element pontos = novaVia.get(i);
+//				novaVia.remove(novaVia.get(i));
+//				writePontoDeMedida(pontos, listaDePontosDeMedida, doc, novaVia);
+//			}
+//		}
+//	}
 }
--- src/test/java/railwayGenerator/RailwayGeneratorRunner.java
+++ src/test/java/railwayGenerator/RailwayGeneratorRunner.java
@@ -7,11 +7,11 @@ public class RailwayGeneratorRunner
 		RailwayGeneratorRequest req = RailwayGeneratorRequest
 				.builder()
 				.filePath("/Users/inkt/dev/tcc/railwayGenerator/src/test/resources/viaBase.xml")
-				.qtdCurvas(100)
-				.qtdRetas(2000)
-				.qtdAclives(478)
-				.qtdDeclives(470)
-				.qtdPlanos(2)
+//				.qtdCurvas(100)
+//				.qtdRetas(2000)
+				.qtdAclives(482)
+				.qtdDeclives(469)
+//				.qtdPlanos(7)
 				.qtdCristas(0)
 				.build();
 		
